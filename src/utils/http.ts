8c300b75155a9ed442fd9129cd8649022e5778a8
Rion <zya@snsbj.com>
2023-02-01 11:42:47 +0800

    ci: add utils/http

---
+++ src/utils/http.ts
@@ -0,0 +1,44 @@
+import qs from "qs";
+import * as auth from "auth-provide";
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
+interface Config extends RequestInit {
+  data?: object;
+  token?: string;
+}
+
+export const Http = async (
+  endpoint: string,
+  { data, token, headers, ...customConfig }: Config
+) => {
+  const config = {
+    // customConfig内如果包含method，会覆盖缺省值
+    method: "GET",
+    headers: {
+      Authorization: token ? `Bearer ${token}` : "",
+      "Content-Type": data ? "application/json" : "",
+    },
+    ...customConfig,
+  };
+
+  if (config.method.toUpperCase() === "GET") {
+    endpoint += `?${qs.stringify(data)}`;
+  } else {
+    config.body = JSON.stringify(data || {});
+  }
+
+  return fetch(`${apiUrl}/${endpoint}`, config).then(async (res) => {
+    if (res.status === 401) {
+      await auth.logout();
+      window.location.reload();
+      return Promise.reject({ msg: "重新登录" });
+    }
+    const json = await res.json();
+    if (res.ok) {
+      return json;
+    } else {
+      return Promise.reject({ data: json });
+    }
+  });
+};

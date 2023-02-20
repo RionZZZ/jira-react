2eb5d99ac3fb778114bca501680a6ab8e45ff329
Rion <zya@snsbj.com>
2023-02-20 14:27:31 +0800

    feat: add id-select component

---
+++ src/components/id-select.tsx
@@ -0,0 +1,36 @@
+import { Select } from "antd";
+import React from "react";
+import { Raw } from "types";
+
+type SelectProps = React.ComponentProps<typeof Select>;
+
+// interface IdSelectProps extends SelectProps {
+interface IdSelectProps
+  extends Omit<SelectProps, "value" | "onChange" | "options"> {
+  value: Raw | undefined;
+  onChange: (value?: number) => void;
+  defaultOptionName?: string;
+  options?: { name: string; id: number }[];
+}
+
+export const IdSelect: React.FC<IdSelectProps> = (props) => {
+  const { value, onChange, options, defaultOptionName, ...restProps } = props;
+  return (
+    <Select
+      value={toNumber(value)}
+      onChange={(value) => onChange(toNumber(value) || undefined)}
+      {...restProps}
+    >
+      {defaultOptionName ? (
+        <Select.Option value={0}>{defaultOptionName}</Select.Option>
+      ) : null}
+      {options?.map((option) => (
+        <Select.Option key={option.id} value={option.id}>
+          {option.name}
+        </Select.Option>
+      ))}
+    </Select>
+  );
+};
+
+const toNumber = (value: unknown) => (isNaN(Number(value)) ? 0 : Number(value));
